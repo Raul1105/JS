08b922fa76d6310953c150d9bd04f79c7ada0fbb
Raul1105 <113746444+Raul1105@users.noreply.github.com>
2025-06-12 04:35:29 -0300

    Atualização

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,27 @@
 let numero = document.querySelectorAll('td');
 let texto = document.querySelector('#resultado');
 let enviar = document.querySelector('#enviar');
+let numero1;
+
+
 
 
 numero.forEach((num) =>{
     num.addEventListener('click', () => {
-        let n = num.textContent
-        texto.innerHTML += n;
+        if (num.textContent != 'Enter'){  
+            numero1 = num.textContent;
+            texto.innerHTML += numero1;
+            console.log(numero1);
+        }
+
     })
+    
 })
 
 enviar.addEventListener('click', () => {
+
+    texto.innerHTML = numero1 * 2;
+    console.log(`O tipo é ${typeof(numero1)} e seu valor ${numero1}`)
+    console.log(parseInt(numero1))
     
 })
